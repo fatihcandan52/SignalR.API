c25c72782911cfd2ed660a2550bde40d90529ed9
fatihcandan52 <fatihcandan52@gmail.com>
2020-06-27 12:16:34 +0300

    Web project initial commit

--- SignalR.Web/Startup.cs
+++ SignalR.Web/Startup.cs
@@ -1,11 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SignalR.API.Hubs;
 
-namespace SignalR.API
+namespace SignalR.Web
 {
     public class Startup
     {
@@ -19,8 +23,7 @@ namespace SignalR.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddSignalR();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +33,14 @@ namespace SignalR.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -39,10 +48,9 @@ namespace SignalR.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-
-                //http://localhost:4400/MyHub
-                endpoints.MapHub<MyHub>("/MyHub");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
